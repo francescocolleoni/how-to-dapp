7db77c8add3038290e3b585d9d9756030ebb3936
Francesco Colleoni <wolfra@gmail.com>
2022-07-07 13:34:19 +0200

    Tutorial app implementation.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+
 import './App.css';
 
+const greeterAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
 const App = () => {
+  const [greeting, setGreetingValue] = useState();
+
+  const requestAccount = async () => {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  const fetchGreeting = async () => {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider);
+      try {
+        const data = await contract.greet();
+        console.log('data: ', data);
+      } catch (err) {
+        console.log("Error: ", err);
+      }
+    }
+  }
+
+  const setGreeting = async () => {
+    if (!greeting) {
+      return;
+    }
+
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(greeting);
+      await transaction.wait();
+      fetchGreeting();
+    }
+  }
+
   return (
-    <div>How to DApp (?)</div>
+    <div>
+      <h3>How to DApp (?)</h3>
+  
+      <button onClick={fetchGreeting}>Fetch Greeting</button>
+      <button onClick={setGreeting}>Set Greeting</button>
+      <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
+    </div>
   );
 }
 
